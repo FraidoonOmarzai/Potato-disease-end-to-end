ff3cabac49b06bd82942fcd69080b60d03e71cb8
FraidoonOmarzai <fraidoon_omarzai@yahoo.com>
2023-12-16 15:44:07 +0000

    data validation completed

--- src/PotatoDisease/config/configuration.py
+++ src/PotatoDisease/config/configuration.py
@@ -1,6 +1,7 @@
 from PotatoDisease.constants import *
 from PotatoDisease.utils.common import read_yaml, create_directories
-from PotatoDisease.entity.config_entity import (DataIngestionConfig)
+from PotatoDisease.entity.config_entity import (DataIngestionConfig,
+                                                DataValidationConfig)
 
 
 class ConfigurationManager:
@@ -21,3 +22,14 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+
+    def get_data_validation(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            unzip_dir=config.unzip_dir,
+            status=config.status
+        )
+        return data_validation_config
